0eaccd2cf2f7d6d1b00c162a72774fe6d487c644
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-06-24 01:12:27 +0100

    Corrected the debug default value

--- blogpro/settings.py
+++ blogpro/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG", "F alse").lower() == "true" )
+DEBUG = str(os.getenv("DEBUG", "False").lower() == "true" )
 
 ALLOWED_HOSTS = str(os.getenv("ALLOWED_HOSTS").split(" "))
 
